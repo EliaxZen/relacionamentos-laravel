fd0f2c53eb849e577d98be10afcf1f281143c2ca
Elias Galvão Alves <galvaoalveselias@gmail.com>
2024-08-23 11:11:55 -0300

    configurando controller

--- database/migrations/2024_08_23_134742_create_addresses_table.php
+++ database/migrations/2024_08_23_134742_create_addresses_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('addresses', function (Blueprint $table) {
             $table->id();
+            $table->string('adress');
             $table->timestamps();
         });
     }
