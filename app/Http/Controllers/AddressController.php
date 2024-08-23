abe03e32ff1cca652ebda2270a0dbc69deaddebb
Elias Galvão Alves <galvaoalveselias@gmail.com>
2024-08-23 10:46:42 -0300

    renomeando controller

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class AdressController extends Controller
+class AddressController extends Controller
 {
     //
 }
--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\AdressController;
+use App\Http\Controllers\AddressController;
 
 // Um usuário, inicialmente, tem um endereço.
 Route::get('/users', [UserController::class, 'index']);
 
 
-Route::get('/addresses', [AdressController::class, 'index']);
+Route::get('/addresses', [AddressController::class, 'index']);
