105486421766d82e639b29c50eb41c1fc3db4279
Elias Galvão Alves <galvaoalveselias@gmail.com>
2024-08-23 10:43:23 -0300

    criando as rotas e o controller para cada uma

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdressController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// Um usuário, inicialmente, tem um endereço.
+Route::get('/users', [UserController::class, 'index']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+Route::get('/addresses', [AdressController::class, 'index']);
