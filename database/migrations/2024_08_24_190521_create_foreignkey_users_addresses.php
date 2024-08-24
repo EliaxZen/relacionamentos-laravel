b37419c028a38cc42ef58c6dbc4c516d07c61843
Elias Galvão Alves <galvaoalveselias@gmail.com>
2024-08-24 18:15:25 -0300

    configurando corretamente o metodo down na migrate

--- database/migrations/2024_08_24_190521_create_foreignkey_users_addresses.php
+++ database/migrations/2024_08_24_190521_create_foreignkey_users_addresses.php
@@ -15,7 +15,10 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             //
-            $table->foreign('address_id')->references('id')->on('addresses');
+            $table->foreign('address_id')
+            ->references('id')
+            ->on('addresses')
+            ->onDelete('CASCADE');
         });
     }
 
@@ -28,6 +31,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             //
+            $table->dropForeign(['address_id']);
         });
     }
 };
