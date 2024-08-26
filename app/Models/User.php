6097e4305441e0185dd09e39ac32483cf4c57862
Elias Galvão Alves <galvaoalveselias@gmail.com>
2024-08-26 09:57:53 -0300

    reestruturando tabelas [2]

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,7 +43,5 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function address(){
-        return $this->hasOne(Address::class, 'id', 'address_id');
-    }
+
 }
