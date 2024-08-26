1d81dd0d68ea657cb88e3c994bac761de0cfb776
Elias Galvão Alves <galvaoalveselias@gmail.com>
2024-08-26 10:36:47 -0300

    relacionamentos has many

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,5 +43,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    public function address(){
+        return $this->hasMany(Address::class);
+    }
+
 
 }
